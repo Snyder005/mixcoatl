beaccec4dc7ca2600836c503755b3ae2cb7435e6
Snyder005 <snyder18@stanford.edu>
2020-08-24 16:40:12 -0700

    Changes to crosstalk code to facilitate more efficient runs: CrosstalkMatrix rewritten, removed in task calibration of images.

--- python/mixcoatl/crosstalkTask.py
+++ python/mixcoatl/crosstalkTask.py
@@ -1,4 +1,3 @@
-import os
 import numpy as np
 from scipy.ndimage.filters import gaussian_filter
 import lsst.pex.config as pexConfig
@@ -7,8 +6,7 @@ import lsst.pipe.base as pipeBase
 import lsst.eotest.image_utils as imutils
 from lsst.eotest.sensor.MaskedCCD import MaskedCCD
 
-from mixcoatl.crosstalk import make_stamp, calibrated_stack, crosstalk_model_fit 
-from mixcoatl.crosstalk import CrosstalkMatrix
+from mixcoatl.crosstalk import CrosstalkMatrix, make_stamp, crosstalk_fit
 
 class CrosstalkConfig(pexConfig.Config):
     
@@ -27,67 +25,67 @@ class CrosstalkTask(pipeBase.Task):
     ConfigClass = CrosstalkConfig
     _DefaultName = "CrosstalkTask"
 
-    @pipeBase.timeMethod
-    def run(self, sensor_id1, infiles1, gains1, bias_frame1=None, dark_frame1=None,
+    def run(self, sensor_id1, infile1, gains1, bias_frame1=None, dark_frame1=None,
             crosstalk_matrix_file=None, **kwargs):
 
-        ## Parse kwargs for separate victim parameters
+        ## Parse kwargs for separate victim CCD inputs
         try:
             sensor_id2 = kwargs['sensor_id2']
         except KeyError:
             sensor_id2 = sensor_id1
-            infiles2 = infiles1
+            infile2 = infile1
             gains2 = gains1
             bias_frame2 = bias_frame1
             dark_frame2 = dark_frame1
         else:
-            infiles2 = kwargs['infiles2']
+            infile2 = kwargs['infile2']
             gains2 = kwargs['gains2']
             bias_frame2 = kwargs['bias_frame2']
             dark_frame2 = kwargs['dark_frame2']
-        
-        ccds1 = [MaskedCCD(infile, bias_frame=bias_frame1, 
-                           dark_frame=dark_frame1) for infile in infiles1]
-        ccds2 = [MaskedCCD(infile, bias_frame=bias_frame2,
-                           dark_frame=dark_frame2) for infile in infiles2]
 
-        all_amps = imutils.allAmps(infiles1[0])
+        ccd1 = MaskedCCD(infile1, bias_frame=bias_frame1, dark_frame=dark_frame1)
+        ccd2 = MaskedCCD(infile2, bias_frame=bias_frame2, dark_frame=dark_frame2)
+        
+        all_amps = imutils.allAmps(infile1)
 
         ## Create new matrix or modify existing
         if crosstalk_matrix_file is not None:
-            crosstalk_matrix = CrosstalkMatrix(sensor_id1, sensor_id2, 
-                                               crosstalk_matrix_file, 
+            crosstalk_matrix = CrosstalkMatrix(sensor_id1, victim_id=sensor_id2,
+                                               filename=crosstalk_matrix_file,
                                                namps=len(all_amps))
-            output_file = crosstalk_matrix_file
+            outfile = crosstalk_matrix_file
         else:
-            output_file = os.path.join(self.config.output_file)
-            crosstalk_matrix = CrosstalkMatrix(sensor_id1, sensor_id2, 
+            outfile = self.config.outfile
+            crosstalk_matrix = CrosstalkMatrix(sensor_id1, victim_id=sensor_id2,
                                                namps=len(all_amps))
 
         num_aggressors = 0
 
-        ## Search each amp for aggressor spot
-        for amp in all_amps:
-            imarr1 = calibrated_stack(ccds1, amp, gain=gains1[amp])
+        ## Search each amp for aggressor
+        for i in all_amps:
+            imarr1 = ccd1.unbiased_and_trimmed_image(i).getImage().getArray()*gains1[i]
             smoothed = gaussian_filter(imarr1, 20)
             y, x = np.unravel_index(smoothed.argmax(), smoothed.shape)
-            Y, X = np.ogrid[-y:smoothed.shape[0]-y, -x:smoothed.shape[1]-x]
-            mask = X*X + Y*Y >= 20*20
-            test = np.ma.MaskedArray(imarr1, mask)
+            stamp1 = make_stamp(imarr1, y, x)
+            ly, lx = stamp1.shape
+            Y, X = np.ogrid[-ly/2:ly/2, -lx/2:lx/2]
+            mask = X*X + Y*Y <= 20*20
 
-            ## Verify signal threshold
-            if np.mean(test) > self.config.threshold:
-                stamp1 = make_stamp(imarr1, y, x)
+            if np.mean(stamp1[mask]) > self.config.threshold:
+
                 row = {}
 
                 ## Calculate crosstalk for each victim amp
-                for i in all_amps:
-                    imarr2 = calibrated_stack(ccds2, i, gain=gains2[i])
+                for j in all_amps:
+                    imarr2 = ccd2.unbiased_and_trimmed_image(j).getImage().getArray()*gains2[j]
+
                     stamp2 = make_stamp(imarr2, y, x)
-                    row[i] = crosstalk_model_fit(stamp1, stamp2, noise=7.0,
+                    row[j] = crosstalk_fit(stamp1, stamp2, noise=7.0,
                                                  num_iter=self.config.num_iter,
                                                  nsig=self.config.nsig)
-                crosstalk_matrix.set_row(amp, row)
+
+                crosstalk_matrix.set_row(i, row)
                 if num_aggressors == 4: break
 
-        crosstalk_matrix.write_fits(output_file)
+        crosstalk_matrix.write_fits(outfile)
+   
