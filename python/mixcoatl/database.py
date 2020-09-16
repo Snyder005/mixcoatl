55e58cb596e83c5badbf46db7b078368c3ed6602
Snyder005 <snyder18@stanford.edu>
2020-09-16 18:11:16 +0000

    Added factory methods for Segment and Sensor.  Added convenince function to query database for results.

--- python/mixcoatl/database.py
+++ python/mixcoatl/database.py
@@ -1,7 +1,8 @@
 import sqlalchemy as sql
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship, backref, sessionmaker
+from sqlalchemy.orm import relationship, backref, sessionmaker, aliased
 from contextlib import contextmanager
+from sqlalchemy.orm.exc import NoResultFound
 
 from mixcoatl.errors import AlreadyExists, MissingKeyword
 
@@ -9,15 +10,14 @@ Base = declarative_base()
 Session = sessionmaker()
 
 @contextmanager
-def db_session(database):
+def db_session(database, echo=False):
     """Create a session bound to the given database.
     
     Args:
         database (str): Database filepath.
     """
-
     try:
-        engine = sql.create_engine(database, echo=False)
+        engine = sql.create_engine('sqlite:///{0}'.format(database), echo=echo)
         Base.metadata.create_all(engine)
         Session.configure(bind=engine)
         session = Session()
@@ -49,7 +49,6 @@ class Result(Base):
     
     def add_to_db(self, session):
         """Add Result to database."""
-        
         session.add(self)
         
 class Segment(Base):
@@ -66,9 +65,26 @@ class Segment(Base):
     results = relationship("Result", back_populates="aggressor", foreign_keys=[Result.aggressor_id])
     sensor = relationship("Sensor", back_populates="segments")
     
+    @classmethod
+    def from_db(cls, session, sensor_name, **kwargs):
+        """Initialize Segment from database using a query."""
+        query = session.query(cls)
+
+        ## Query on amplifier number or name
+        if 'amplifier_number' in kwargs:
+            query = query.filter(cls.amplifier_number == kwargs['amplifier_number'])
+        elif 'name' in kwargs:
+            query = query.filter(cls.name == kwargs['name'])
+        else:
+            raise MissingKeyword('Missing amplifier_number or name keyword for query.')
+
+	## Filter on sensor name
+        query = query.join(Sensor).filter(Sensor.name == sensor_name)
+
+        return query.one()
+
     def add_to_db(self, session):
         """Add Segment to database."""
-        
         session.add(self)
 
 class Sensor(Base):
@@ -85,27 +101,30 @@ class Sensor(Base):
     segments = relationship("Segment", back_populates="sensor")
     
     @classmethod
-    def from_db(cls, session, **kwargs):
+    def from_db(cls, session, sensor_name):
+        """Initialize Sensor from database using a query."""
+        query = session.query(cls).filter(cls.name == sensor_name)
         
-        query = session.query(cls)
-        
-        ## Query on name or id
-        if 'name' in kwargs:
-            query = query.filter(cls.name == kwargs['name'])
-        elif 'id' in kwargs:
-            query = query.filter(cls.id == kwargs['id'])
-        else:
-            raise MissingKeyword('Query requires name or id keyword.')
-            
-        sensor = query.one()
-        return sensor
+        return query.one()
             
     def add_to_db(self, session):
         """Add Sensor to database."""
-        
-        ## Check if already exists in database
-        sensor = session.query(Sensor).filter_by(name=self.name).first()
-        if sensor is None:
-            session.add(self)
-        else:
-            raise AlreadyExists('Sensor already exists in database.')
+        session.add(self)
+
+def query_results(session, sensor_name, aggressor_amp, victim_amp, methods=None):
+    """Query database for results."""
+    a1 = aliased(Segment)
+    a2 = aliased(Segment)
+    
+    query = session.query(Result).join(a1, Result.aggressor_id == a1.id).\
+        join(a2, Result.victim_id == a2.id).\
+        filter(a1.amplifier_number == aggressor_amp).\
+        filter(a2.amplifier_number == victim_amp).\
+        join(Sensor).filter(Sensor.name == sensor_name)
+
+    if methods is not None:
+        if not isinstance(methods, list):
+            methods = [methods]
+        query = query.filter(Result.method.in_(methods))
+
+    return query.all()
