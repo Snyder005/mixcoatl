4edcc1aa05d8ebf8a1404088dd5e69fbb5a44766
Snyder005 <snyder18@stanford.edu>
2021-01-26 11:18:32 -0800

    Fixed amplifier-to-segment mapping (and vice-versa) to match LCA-13381-B detector plane layout documentation.

--- python/mixcoatl/utils.py
+++ python/mixcoatl/utils.py
@@ -23,12 +23,12 @@ E2V_AMP_GEOM = AmplifierGeometry(prescan=10, nx=512, ny=2002,
                                  amp_loc=amp_loc['E2V'], vendor='E2V')
 """AmplifierGeometry: Amplifier geometry parameters for LSST E2V CCD sensors."""
 
-AMP2SEG = {1 : 'C17', 2 : 'C16', 3 : 'C15', 4 : 'C14', 5 : 'C13', 6 : 'C12', 7 : 'C11', 8 : 'C10',
-           9 : 'C00', 10 : 'C01', 11 : 'C02', 12 : 'C03', 13 : 'C04', 14 : 'C05', 15 : 'C06', 16 : 'C07'}
+AMP2SEG = {1 : 'C10', 2 : 'C11', 3 : 'C12', 4 : 'C13', 5 : 'C14', 6 : 'C15', 7 : 'C16', 8 : 'C17',
+           9 : 'C07', 10 : 'C06', 11 : 'C05', 12 : 'C04', 13 : 'C03', 14 : 'C02', 15 : 'C01', 16 : 'C00'}
 """dict: Dictionary mapping from CCD amplifier number to segment names."""
 
-SEG2AMP = {'C00' : 9, 'C01' : 10, 'C02' : 11, 'C03' : 12, 'C04' : 13, 'C05' : 14, 'C06' : 15, 'C07' : 16,
-           'C10' : 8, 'C11' : 7, 'C12' : 6, 'C13' : 5, 'C14' : 4, 'C15' : 3, 'C16' : 2, 'C17' : 1}
+SEG2AMP = {'C00' : 16, 'C01' : 15, 'C02' : 14, 'C03' : 13, 'C04' : 12, 'C05' : 11, 'C06' : 10, 'C07' : 9,
+           'C10' : 1, 'C11' : 2, 'C12' : 3, 'C13' : 4, 'C14' : 5, 'C15' : 6, 'C16' : 7, 'C17' : 8}
 """dict: Dictionary mapping from CCD segment names to amplifier number."""
 
 def calibrated_stack(infiles, outfile, bias_frame=None, dark_frame=None, 
