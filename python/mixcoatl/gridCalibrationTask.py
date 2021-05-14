Cleaned up code syntax in gridFitTask and fixed calculation of normalized centroid shifts. Updated gridCalibrationTask implementation to use Astropy Table.

python/mixcoatl/gridCalibrationTask.py
@@ -1,107 +1,83 @@
-import sys
 import numpy as np
-from astropy.io import fits
 from astropy.table import Table, vstack
 import lsst.pex.config as pexConfig
 import lsst.pipe.base as pipeBase
 import lsst.pipe.base.connectionTypes as cT
 
-from mixcoatl.sourcegrid import DistortedGrid
-
 cols_to_aggregate = ['spotgrid_index',
-                    'base_SdssCentroid_xErr',
-                    'base_SdssCentroid_yErr',
-                    'base_Blendedness_old',
-                    'base_Blendedness_raw',
-                    'base_Blendedness_raw_child_instFlux',
-                    'base_Blendedness_raw_parent_instFlux',
-                    'base_Blendedness_abs',
-                    'base_Blendedness_abs_child_instFlux',
-                    'base_Blendedness_abs_parent_instFlux',
-                    'base_Blendedness_raw_child_xx',
-                    'base_Blendedness_raw_child_yy',
-                    'base_Blendedness_raw_child_xy',
-                    'base_Blendedness_raw_parent_xx',
-                    'base_Blendedness_raw_parent_yy',
-                    'base_Blendedness_raw_parent_xy',
-                    'base_Blendedness_abs_child_xx',
-                    'base_Blendedness_abs_child_yy',
-                    'base_Blendedness_abs_child_xy',
-                    'base_Blendedness_abs_parent_xx',
-                    'base_Blendedness_abs_parent_yy',
-                    'base_Blendedness_abs_parent_xy',
-                    'base_SdssShape_xx',
-                    'base_SdssShape_yy',
-                    'base_SdssShape_xy',
-                    'base_SdssShape_xxErr',
-                    'base_SdssShape_yyErr',
-                    'base_SdssShape_xyErr',
-                    'base_SdssShape_x',
-                    'base_SdssShape_y',
-                    'base_SdssShape_instFlux',
-                    'base_SdssShape_instFluxErr',
-                    'base_SdssShape_psf_xx',
-                    'base_SdssShape_psf_yy',
-                    'base_SdssShape_psf_xy',
-                    'base_SdssShape_instFlux_xx_Cov',
-                    'base_SdssShape_instFlux_yy_Cov',
-                    'base_SdssShape_instFlux_xy_Cov',
-                    'ext_shapeHSM_HsmPsfMoments_x',
-                    'ext_shapeHSM_HsmPsfMoments_y',
-                    'ext_shapeHSM_HsmPsfMoments_xx',
-                    'ext_shapeHSM_HsmPsfMoments_yy',
-                    'ext_shapeHSM_HsmPsfMoments_xy',
-                    'ext_shapeHSM_HsmShapeBj_e1',
-                    'ext_shapeHSM_HsmShapeBj_e2',
-                    'ext_shapeHSM_HsmShapeBj_sigma',
-                    'ext_shapeHSM_HsmShapeBj_resolution',
-                    'ext_shapeHSM_HsmShapeKsb_g1',
-                    'ext_shapeHSM_HsmShapeKsb_g2',
-                    'ext_shapeHSM_HsmShapeKsb_sigma',
-                    'ext_shapeHSM_HsmShapeKsb_resolution',
-                    'ext_shapeHSM_HsmShapeLinear_e1',
-                    'ext_shapeHSM_HsmShapeLinear_e2',
-                    'ext_shapeHSM_HsmShapeLinear_sigma',
-                    'ext_shapeHSM_HsmShapeLinear_resolution',
-                    'ext_shapeHSM_HsmShapeRegauss_e1',
-                    'ext_shapeHSM_HsmShapeRegauss_e2',
-                    'ext_shapeHSM_HsmShapeRegauss_sigma',
-                    'ext_shapeHSM_HsmShapeRegauss_resolution',
-                    'ext_shapeHSM_HsmSourceMoments_x',
-                    'ext_shapeHSM_HsmSourceMoments_y',
-                    'ext_shapeHSM_HsmSourceMoments_xx',
-                    'ext_shapeHSM_HsmSourceMoments_yy',
-                    'ext_shapeHSM_HsmSourceMoments_xy',
-                    'base_CircularApertureFlux_3_0_instFlux',
-                    'base_CircularApertureFlux_3_0_instFluxErr',
-                    'base_CircularApertureFlux_4_5_instFlux',
-                    'base_CircularApertureFlux_4_5_instFluxErr',
-                    'base_CircularApertureFlux_6_0_instFlux',
-                    'base_CircularApertureFlux_6_0_instFluxErr',
-                    'base_CircularApertureFlux_9_0_instFlux',
-                    'base_CircularApertureFlux_9_0_instFluxErr',
-                    'base_CircularApertureFlux_12_0_instFlux',
-                    'base_CircularApertureFlux_12_0_instFluxErr',
-                    'base_CircularApertureFlux_17_0_instFlux',
-                    'base_CircularApertureFlux_17_0_instFluxErr',
-                    'base_CircularApertureFlux_25_0_instFlux',
-                    'base_CircularApertureFlux_25_0_instFluxErr',
-                    'base_CircularApertureFlux_35_0_instFlux',
-                    'base_CircularApertureFlux_35_0_instFluxErr',
-                    'base_CircularApertureFlux_50_0_instFlux',
-                    'base_CircularApertureFlux_50_0_instFluxErr',
-                    'base_CircularApertureFlux_70_0_instFlux',
-                    'base_CircularApertureFlux_70_0_instFluxErr',
-                    'base_GaussianFlux_instFlux',
-                    'base_GaussianFlux_instFluxErr',
-                    'base_LocalBackground_instFlux',
-                    'base_LocalBackground_instFluxErr',
-                    'base_PsfFlux_instFlux',
-                    'base_PsfFlux_instFluxErr',
-                    'base_PsfFlux_area',
-                    'base_Variance_value',
-                    'base_ClassificationExtendedness_value',
-                    'base_FootprintArea_value']
+                     'spotgrid_normalized_dy',
+                     'spotgrid_normalized_dx',
+                     'base_SdssCentroid_xErr',
+                     'base_SdssCentroid_yErr',
+                     'base_SdssShape_xx',
+                     'base_SdssShape_yy',
+                     'base_SdssShape_xy',
+                     'base_SdssShape_xxErr',
+                     'base_SdssShape_yyErr',
+                     'base_SdssShape_xyErr',
+                     'base_SdssShape_x',
+                     'base_SdssShape_y',
+                     'base_SdssShape_instFlux',
+                     'base_SdssShape_instFluxErr',
+                     'base_SdssShape_psf_xx',
+                     'base_SdssShape_psf_yy',
+                     'base_SdssShape_psf_xy',
+                     'base_SdssShape_instFlux_xx_Cov',
+                     'base_SdssShape_instFlux_yy_Cov',
+                     'base_SdssShape_instFlux_xy_Cov',
+                     'ext_shapeHSM_HsmPsfMoments_x',
+                     'ext_shapeHSM_HsmPsfMoments_y',
+                     'ext_shapeHSM_HsmPsfMoments_xx',
+                     'ext_shapeHSM_HsmPsfMoments_yy',
+                     'ext_shapeHSM_HsmPsfMoments_xy',
+                     'ext_shapeHSM_HsmShapeBj_e1',
+                     'ext_shapeHSM_HsmShapeBj_e2',
+                     'ext_shapeHSM_HsmShapeBj_sigma',
+                     'ext_shapeHSM_HsmShapeBj_resolution',
+                     'ext_shapeHSM_HsmShapeKsb_g1',
+                     'ext_shapeHSM_HsmShapeKsb_g2',
+                     'ext_shapeHSM_HsmShapeKsb_sigma',
+                     'ext_shapeHSM_HsmShapeKsb_resolution',
+                     'ext_shapeHSM_HsmShapeLinear_e1',
+                     'ext_shapeHSM_HsmShapeLinear_e2',
+                     'ext_shapeHSM_HsmShapeLinear_sigma',
+                     'ext_shapeHSM_HsmShapeLinear_resolution',
+                     'ext_shapeHSM_HsmShapeRegauss_e1',
+                     'ext_shapeHSM_HsmShapeRegauss_e2',
+                     'ext_shapeHSM_HsmShapeRegauss_sigma',
+                     'ext_shapeHSM_HsmShapeRegauss_resolution',
+                     'ext_shapeHSM_HsmSourceMoments_x',
+                     'ext_shapeHSM_HsmSourceMoments_y',
+                     'ext_shapeHSM_HsmSourceMoments_xx',
+                     'ext_shapeHSM_HsmSourceMoments_yy',
+                     'ext_shapeHSM_HsmSourceMoments_xy',
+                     'base_CircularApertureFlux_3_0_instFlux',
+                     'base_CircularApertureFlux_3_0_instFluxErr',
+                     'base_CircularApertureFlux_4_5_instFlux',
+                     'base_CircularApertureFlux_4_5_instFluxErr',
+                     'base_CircularApertureFlux_6_0_instFlux',
+                     'base_CircularApertureFlux_6_0_instFluxErr',
+                     'base_CircularApertureFlux_9_0_instFlux',
+                     'base_CircularApertureFlux_9_0_instFluxErr',
+                     'base_CircularApertureFlux_12_0_instFlux',
+                     'base_CircularApertureFlux_12_0_instFluxErr',
+                     'base_CircularApertureFlux_17_0_instFlux',
+                     'base_CircularApertureFlux_17_0_instFluxErr',
+                     'base_CircularApertureFlux_25_0_instFlux',
+                     'base_CircularApertureFlux_25_0_instFluxErr',
+                     'base_CircularApertureFlux_35_0_instFlux',
+                     'base_CircularApertureFlux_35_0_instFluxErr',
+                     'base_CircularApertureFlux_50_0_instFlux',
+                     'base_CircularApertureFlux_50_0_instFluxErr',
+                     'base_CircularApertureFlux_70_0_instFlux',
+                     'base_CircularApertureFlux_70_0_instFluxErr',
+                     'base_GaussianFlux_instFlux',
+                     'base_GaussianFlux_instFluxErr',
+                     'base_PsfFlux_instFlux',
+                     'base_PsfFlux_instFluxErr',
+                     'base_PsfFlux_area',
+                     'base_ClassificationExtendedness_value',
+                     'base_FootprintArea_value']
 
 class GridCalibrationConnections(pipeBase.PipelineTaskConnections,
                                  dimensions=("instrument", "detector")):
@@ -123,11 +99,11 @@ class GridCalibrationConnections(pipeBase.PipelineTaskConnections,
     )
 
 class GridCalibrationConfig(pipeBase.PipelineTaskConfig,
-                            pipelineConnections=GridCalibationConnections):
+                            pipelineConnections=GridCalibrationConnections):
     """Configuration for Calibration Task"""
     
-    ncols = pexConfig.Field("Number of ideal grid columns", int, default=49)
-    nrows = pexConfig.Field("Number of ideal grid rows", int, default=49)
+    numCols = pexConfig.Field("Number of ideal grid columns", int, default=49)
+    numRows = pexConfig.Field("Number of ideal grid rows", int, default=49)
 
 class GridCalibrationTask(pipeBase.PipelineTask):
     """Task to combine distorted grid fits."""
@@ -137,53 +113,41 @@ class GridCalibrationTask(pipeBase.PipelineTask):
 
     def run(self, inputCatalogs):
 
-        ## Initialize data from distorted_grid
-        all_x0 = np.zeros(len(infiles))
-        all_y0 = np.zeros(len(infiles))
-        all_xstep = np.zeros(len(infiles))
-        all_ystep = np.zeros(len(infiles))
-        all_theta = np.zeros(len(infiles))
+        ## Grid parameters
+        all_x0 = np.zeros(len(inputCatalogs))
+        all_y0 = np.zeros(len(inputCatalogs))
+        all_xstep = np.zeros(len(inputCatalogs))
+        all_ystep = np.zeros(len(inputCatalogs))
+        all_theta = np.zeros(len(inputCatalogs))
         all_data_tables = []
 
-        for i, infile in enumerate(infiles):
-            grid = DistortedGrid.from_fits(infile)
+        for i, inputCat in enumerate(inputCatalogs):
+            
+            md = inputCat.getMetadata()
 
-            all_x0[i] = grid.x0
-            all_y0[i] = grid.y0
-            all_xstep[i] = grid.xstep
-            all_ystep[i] = grid.ystep
-            all_theta[i] = grid.theta
+            if (md['GRID_NROWS'],  md['GRID_NCOLS']) != (self.config.numRows, self.config.numCols):
+                raise ValueError('Grid sizes do not match: ({0}, {1}), ({1}, {2})'\
+                    .format(md['GRID_NROWS'], md['GRID_NCOLS'], self.config.numRows, self.config.numCols))
 
-            with fits.open(infile) as hdulist:
-                all_data_tables.append(Table(hdulist[1].data))
-
-
+            all_x0[i] = md['GRID_X0']
+            all_y0[i] = md['GRID_Y0']
+            all_xstep[i] = md['GRID_XSTEP']
+            all_ystep[i] = md['GRID_YSTEP']
+            all_theta[i] = md['GRID_THETA']
+            
+            all_data_tables.append(inputCat.asAstropy())
+            
         ## Compute data means
-        mean_x0 = np.nanmean(all_x0)
-        mean_y0 = np.nanmean(all_y0)
-        mean_xstep = np.nanmean(all_xstep)
-        mean_ystep = np.nanmean(all_ystep)
-        mean_theta = np.nanmean(all_theta)
-        mean_data_table = vstack(all_data_tables, join_type='exact').group_by('spotgrid_index')
-        mean_data_table = mean_data_table[cols_to_aggregate].groups.aggregate(np.nanmean)
+        meta = {'GRID_X0' : np.nanmean(all_x0),
+                'GRID_Y0' : np.nanmean(all_y0),
+                'GRID_XSTEP' : np.nanmean(all_xstep),
+                'GRID_YSTEP' : np.nanmean(all_ystep),
+                'GRID_THETA' : np.nanmean(all_theta),
+                'GRID_NROWS' : self.config.numRows,
+                'GRID_NCOLS' : self.config.numCols}
+        outputTable = vstack(all_data_tables, join_type='exact').group_by('spotgrid_index')
+        outputTable = outputTable[cols_to_aggregate].groups.aggregate(np.nanmean)
+        outputTable.remove_rows(np.where(outputTable['spotgrid_index'] < 0)[0])
+        outputTable.meta.update(meta)
 
-
-        ## Create and save optical distortions grid
-        grid = DistortedGrid(mean_ystep, mean_xstep, mean_theta, mean_y0, mean_x0, 
-                             self.config.ncols, self.config.nrows)
-        grid.make_source_grid()
-        gY = grid._y
-        gX = grid._x
-
-        optics_grid = DistortedGrid(mean_ystep, mean_xstep, mean_theta, mean_y0, mean_x0, 
-                                    self.config.ncols, self.config.nrows, (gY,gX))
-
-        prihdr = fits.Header()
-        prihdu = fits.PrimaryHDU(header=prihdr)
-        gridhdu = optics_grid.make_grid_hdu()
-        tablehdu = fits.BinTableHDU(mean_data_table)
-
-        hdulist = fits.HDUList([prihdu, tablehdu, gridhdu])
-        hdulist.writeto(os.path.join(self.config.output_dir, self.config.outfile), overwrite=True)
-
-        return
+        return pipeBase.Struct(outputTable=outputTable)

python/mixcoatl/gridFitTask.py
@@ -68,24 +68,25 @@ class GridFitTask(pipeBase.PipelineTask):
     @pipeBase.timeMethod
     def run(self, inputCat, bbox, gridCalibTable=None):
 
-        all_srcY = inputCat['base_SdssCentroid_y']
-        all_srcX = inputCat['base_SdssCentroid_x']
+        all_srcY = inputCat['slot_Centroid_y']
+        all_srcX = inputCat['slot_Centroid_x']
         
-        # Mask the bad grid points
-        quality_mask = (inputCat['base_SdssShape_xx'] > 0.1) \
-                     * (inputCat['base_SdssShape_xx'] < 50.)  \
-                     * (inputCat['base_SdssShape_yy'] > 0.1) \
-                     * (inputCat['base_SdssShape_yy'] < 50.)
+        ## Mask sources by shape
+        quality_mask = (inputCat['slot_Shape_xx'] > 0.1) \
+                     * (inputCat['slot_Shape_xx'] < 50.)  \
+                     * (inputCat['slot_Shape_yy'] > 0.1) \
+                     * (inputCat['slot_Shape_yy'] < 50.)
 
+        ## Mask sources by distance to neighbors
         indices, distances = coordinate_distances(all_srcY, all_srcX, all_srcY, all_srcX)
         outlier_mask = ((distances[:,1] < 100.) & (distances[:,1] > 40.)) & \
             ((distances[:,2] < 100.) & (distances[:,2] > 40.))
 
-        full_mask = quality_mask & outlier_mask
-        srcY = all_srcY[full_mask]
-        srcX = all_srcX[full_mask]
+        mask = quality_mask & outlier_mask
+        srcY = all_srcY[mask]
+        srcX = all_srcX[mask]
         
-        ## Optionally get existing normalized centroid shifts
+        ## Optionally use normalized centroid shifts from calibration
         if gridCalibTable is not None:
             normalized_shifts = (gridCalibTable['spotgrid_normalized_dy'], 
                                  gridCalibTable['spotgrid_normalized_dx'])
@@ -97,41 +98,50 @@ class GridFitTask(pipeBase.PipelineTask):
                                 vary_theta=self.config.varyTheta, normalized_shifts=normalized_shifts,
                                 method=self.config.fitMethod, bbox=bbox)
 
-        ## Match grid to catalog
-        grid_y = np.full(all_srcY.shape[0], np.nan)
-        grid_x = np.full(all_srcX.shape[0], np.nan)
-        grid_norm_dy = np.full(all_srcY.shape[0], np.nan)
-        grid_norm_dx = np.full(all_srcX.shape[0], np.nan)
-        grid_index = np.full(all_srcX.shape[0], np.nan)
-
-        gY, gX = grid.get_source_centroids()
-        closest_indices, closest_distances = coordinate_distances(srcY, srcX, gY, gX)
-        grid_y[full_mask] = gY[closest_indices[:, 0]]
-        grid_x[full_mask] = gX[closest_indices[:, 0]]
-        grid_index[full_mask] = closest_indices[:, 0]
-
-        ## Add spot grid information to new source catalog
+        ## Construct source catalog with new columns
         schema = inputCat.getSchema()
         mapper = afwTable.SchemaMapper(schema)
         mapper.addMinimalSchema(schema, True)
-        grid_y_col = mapper.editOutputSchema().addField('spotgrid_y', type=float,
-                                                        doc='Y-position for ideal spot grid.')
-        grid_x_col = mapper.editOutputSchema().addField('spotgrid_x', type=float,
-                                                        doc='X-position for ideal spot grid.')
-        grid_norm_dy_col = mapper.editOutputSchema().addField('spotgrid_normalized_dy', type=float,
-                                                        doc='Normalized shift from ideal spot grid in Y-position.')
-        grid_norm_dx_col = mapper.editOutputSchema().addField('spotgrid_normalized_dx', type=float,
-                                                        doc='Normalized shift from ideal spot grid in X-position.')
-        grid_index_col = mapper.editOutputSchema().addField('spotgrid_index', type=np.int32,
-                                                            doc='Index of ideal spot grid.')
-    
+        gridYCol = mapper.editOutputSchema().addField('spotgrid_y', type=float,
+                                                      doc='Y-position for spot grid source.')
+        gridXCol = mapper.editOutputSchema().addField('spotgrid_x', type=float,
+                                                      doc='X-position for spot grid source.')
+        normDYCol = mapper.editOutputSchema().addField('spotgrid_normalized_dy', type=float,
+                                                       doc='Normalized shift from spot grid source in Y.')
+        normDXCol = mapper.editOutputSchema().addField('spotgrid_normalized_dx', type=float,
+                                                       doc='Normalized shift from spot grid source in X.')
+        gridIndexCol = mapper.editOutputSchema().addField('spotgrid_index', type=np.int32,
+                                                          doc='Index of corresponding spot grid source.')
+
         outputCat = afwTable.SourceCatalog(mapper.getOutputSchema())
         outputCat.extend(inputCat, mapper=mapper)
-        outputCat[grid_y_col][:] = grid_y
-        outputCat[grid_x_col][:] = grid_x
-        outputCat[grid_norm_dy_col][:] = srcY - grid_y
-        outputCat[grid_norm_dx_col][:] = srcX - grid_x
-        outputCat[grid_index_col][:] = grid_index
+
+        ## Match grid to catalog
+        gridY, gridX = grid.get_source_centroids()
+        match_indices, match_distances = coordinate_distances(srcY, srcX, gridY, gridX)
+
+        ## Construct new column arrays
+        numSrcs = all_srcY.shape[0]
+        all_gridY = np.full(numSrcs, np.nan)
+        all_gridX = np.full(numSrcs, np.nan)
+        all_normDY = np.full(numSrcs, np.nan)
+        all_normDX = np.full(numSrcs, np.nan)
+        all_gridIndex = np.full(numSrcs, np.nan)
+
+        all_gridY[mask] = gridY[match_indices[:, 0]]
+        all_gridX[mask] = gridX[match_indices[:, 0]]
+        all_gridIndex[mask] = match_indices[:, 0]        
+        dy = all_srcY - all_gridY
+        dx = all_srcX - all_gridX
+        all_normDY = (np.sin(-grid.theta)*dx + np.cos(-grid.theta)*dy)/grid.ystep
+        all_normDX = (np.cos(-grid.theta)*dx - np.sin(-grid.theta)*dy)/grid.xstep 
+
+        ## Assign new column arrays to catalog
+        outputCat[gridYCol][:] = all_gridY
+        outputCat[gridXCol][:] = all_gridX
+        outputCat[normDYCol][:] = all_normDY
+        outputCat[normDXCol][:] = all_normDX
+        outputCat[gridIndexCol][:] = all_gridIndex
         
         ## Add grid parameters to metadata
         md = inputCat.getMetadata()
@@ -142,7 +152,6 @@ class GridFitTask(pipeBase.PipelineTask):
         md.add('GRID_YSTEP', grid.ystep)
         md.add('GRID_NCOLS', grid.ncols)
         md.add('GRID_NROWS', grid.nrows)
-
         outputCat.setMetadata(md)
 
         return pipeBase.Struct(gridSourceCat=outputCat)