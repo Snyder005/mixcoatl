a221d9f72f3f6321a5c0993fbf995f87c2036b88
Snyder005 <snyder18@stanford.edu>
2021-06-16 01:08:04 +0000

    Added changes to masking, need to debug.

--- python/mixcoatl/gridFitTask.py
+++ python/mixcoatl/gridFitTask.py
@@ -88,15 +88,10 @@ class GridFitConfig(pipeBase.PipelineTaskConfig,
         default=50.,
         doc="Upper bound on source second moment; used for masking."
     )
-    neighborDistanceLowerBound = Field(
+    neighborDistanceVariation = Field(
         dtype=float,
-        default=60.,
-        doc="Lower bound on distance to nearest source neighbor; used for masking."
-    )
-    neighborDistanceUpperBound = Field(
-        dtype=float,
-        default=70.,
-        doc="Upper bound on distance to nearest source neighbor; used for masking."
+        default=5.,
+        doc="Allowable variation of distance to nearest source neighbor; used for masking."
     )
 
 class GridFitTask(pipeBase.PipelineTask):
@@ -105,26 +100,76 @@ class GridFitTask(pipeBase.PipelineTask):
     _DefaultName = "GridFitTask" 
 
     def run(self, inputCat, bbox, gridCalibTable=None):
-
-        all_srcY = inputCat['slot_Centroid_y']
-        all_srcX = inputCat['slot_Centroid_x']
         
         ## Mask sources by shape
-        quality_mask = (inputCat['slot_Shape_xx'] > self.config.shapeLowerBound) \
-                     * (inputCat['slot_Shape_xx'] < self.config.shapeUpperBound) \
-                     * (inputCat['slot_Shape_yy'] > self.config.shapeLowerBound) \
-                     * (inputCat['slot_Shape_yy'] < self.config.shapeUpperBound)
+        select_by_shape = (inputCat['slot_Shape_xx'] > self.config.shapeLowerBound) \
+                        * (inputCat['slot_Shape_xx'] < self.config.shapeUpperBound) \
+                        * (inputCat['slot_Shape_yy'] > self.config.shapeLowerBound) \
+                        * (inputCat['slot_Shape_yy'] < self.config.shapeUpperBound)
 
-        ## Mask sources by distance to neighbors
+        all_srcY = inputCat['slot_Centroid_y'][select_by_shape]
+        all_srcX = inputCat['slot_Centroid_x'][select_by_shape]
+
+        ## Estimate grid properties
         indices, distances = coordinate_distances(all_srcY, all_srcX, all_srcY, all_srcX)
-        neighbor_check = (distances < self.config.neighborDistanceUpperBound) & \
-                         (distances > self.config.neighborDistanceLowerBound)
-        outlier_mask = np.sum(neighbor_check[:,1:], axis=1) >= 2
+        nn_indices = indices[:, 1:5]
+        nn_distances = distances[:, 1:5]
+        med_distance = np.median(nn_distances)
 
-        mask = quality_mask & outlier_mask
+        nsources = all_srcY.shape[0]
+        dist1_array = np.full(nsources, np.nan)
+        dist2_array = np.full(nsources, np.nan)
+        theta_array = np.full(nsources, np.nan)
+
+        for i in range(nsources):
+
+            yc = all_srcY[i]
+            xc = all_srcX[i]
+
+            for j in range(4):
+
+                nn_dist = nn_distances[i, j]
+                if np.abs(nn_dist - med_distance) > self.config.neighborDistanceVariation: continue
+                y_nn = all_srcY[nn_indices[i, j]]
+                x_nn = all_srcX[nn_indices[i, j]]
+
+                if x_nn > xc:
+                    if y_nn > yc:
+                        dist1_array[i] = nn_dist
+                        theta_array[i] = np.arctan((y_nn-yc)/(x_nn-xc))
+                    else:
+                        dist2_array[i] = nn_dist
+
+        ## Use theta to determine x/y step direction
+        theta = np.nanmedian(theta_array)
+        if theta >= np.pi/4.:
+            theta = theta - (np.pi/2.)
+            xstep = np.nanmedian(dist2_array)
+            ystep = np.nanmedian(dist1_array)
+        else:
+            xstep = np.nanmedian(dist1_array)
+            ystep = np.nanmedian(dist2_array)
+            
+        ## Rotate to pixel x/y
+        R = np.array([[np.cos(-theta), -np.sin(-theta)], [np.sin(-theta), np.cos(-theta)]])
+        points = (R @ np.vstack([all_srcX,all_srcY]))
+        xs = points[0]
+        ys = points[1]
+
+        ## Mask by distance to possible grid vertex
+        mask = np.zeros(len(all_srcX), bool)
+        for i,pt in enumerate(points.T):
+            xs = np.delete(points[0], i)
+            ys = np.delete(points[1], i)
+            if np.any(xs, where=np.isclose(pt[0],xs, rtol=0, atol=self.config.neighborDistanceVariation)) and \
+               np.any(ys, where=np.isclose(pt[1],ys, rtol=0, atol=self.config.neighborDistanceVariation)):
+                mask[i] = True
+            else:
+                mask[i] = False
+                
         srcY = all_srcY[mask]
         srcX = all_srcX[mask]
-        
+       
         ## Optionally use normalized centroid shifts from calibration
         if gridCalibTable is not None:
             gridCalib = DistortedGrid.from_astropy(gridCalibTable)
@@ -133,9 +178,8 @@ class GridFitTask(pipeBase.PipelineTask):
             normalized_shifts = None
             
         ## Perform grid fit
-        grid, result = grid_fit(srcY, srcX, self.config.numColumns, self.config.numRows,
-                                vary_theta=self.config.varyTheta, normalized_shifts=normalized_shifts,
-                                method=self.config.fitMethod, bbox=bbox)
+        grid, result = self.performGridFit(srcY, srcX, ystep, xstep, theta, bbox=bbox,
+                                           normalized_shifts=normalized_shifts)
 
         ## Construct source catalog with new columns
         schema = inputCat.getSchema()
@@ -194,3 +238,47 @@ class GridFitTask(pipeBase.PipelineTask):
         outputCat.setMetadata(md)
 
         return pipeBase.Struct(gridSourceCat=outputCat)
+
+    def performGridFit(self, src_y, src_x, ystep, xstep, theta, normalized_shifts=None, bbox=None):
+
+        ncols = self.config.numCols
+        nrows = self.config.numRows
+
+        ## Find initial guess for grid center based on orientation
+        grid_center_guess = mixCrosstalk.find_midpoint_guess(src_y, src_x, xstep, ystep, theta)
+        y0_guess, x0_guess = grid_center_guess[1], grid_center_guess[0]
+
+        ## Define fit parameters
+        params = Parameters()
+        params.add('ystep', value=ystep, vary=False)
+        params.add('xstep', value=xstep, vary=False)
+        params.add('y0', value=y0_guess, min=y0_guess-3., max=y0_guess+3., vary=True)
+        params.add('x0', value=x0_guess, min=x0_guess-3., max=x0_guess+3., vary=True)
+        params.add('theta', value=theta, min=theta-0.5*np.pi/180., max=theta+0.5*np.pi/180., vary=False)
+
+        minner = Minimizer(mixCrosstalk.fit_error, params, fcn_args=(src_y, src_x, ncols, nrows),
+                           fcn_kws={'normalized_shifts' : normalized_shifts,
+                                    'bbox' : bbox}, nan_policy='omit')
+        result = minner.minimize(params=params, method=self.config.fitMethod, max_nfev=None)
+        x0result = result.params['x0']
+        y0result = result.params['y0']
+
+        if self.config.varyTheta:
+            result_params = result.params
+            result_values = result_params.valuesdict()
+            params['y0'].set(value=result_values['y0'], vary=False)
+            params['x0'].set(value=result_values['x0'], vary=False)
+            params['theta'].set(vary=True)
+            theta_minner = Minimizer(fit_error, params, fcn_args=(src_y, src_x, ncols, nrows),
+                           fcn_kws={'normalized_shifts' : normalized_shifts,
+                                    'bbox' : bbox}, nan_policy='omit')
+            theta_result = theta_minner.minimize(params=params, method=self.config.fitMethod, max_nfev=None)
+            result.params['theta'] = theta_result.params['theta']
+
+        parvals = result.params.valuesdict()
+        grid = mixCrosstalk.DistortedGrid(parvals['ystep'], parvals['xstep'], 
+                                          parvals['theta'], parvals['y0'], 
+                                          parvals['x0'], ncols, nrows, 
+                                          normalized_shifts=normalized_shifts)
+        
+        return grid, result
