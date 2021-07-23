f0f379b9c47e022c4c001dfe43050a194bcdd2d7
Adam Snyder <asnyder@lsst-devl01.ncsa.illinois.edu>
2021-07-23 14:16:01 -0500

    Added ratio error output to crosstalk column task.

--- python/mixcoatl/crosstalkTask.py
+++ python/mixcoatl/crosstalkTask.py
@@ -71,6 +71,12 @@ class CrosstalkColumnConnections(pipeBase.PipelineTaskConnections,
         storageClass="StructuredDataDict",
         dimensions=("instrument", "exposure", "detector"),
     )
+    outputRatioErrors = cT.Output(
+        name="crosstalkRatioErrors",
+        doc="Parameter error on extracted crosstalk pixel ratios.",
+        storageClass="StructuredDataDict",
+        dimensions=("instrument", "exposure", "detector"),
+    )
 
     def __init__(self, *, config=None):
         super().__init__(config=config)
@@ -143,6 +149,7 @@ class CrosstalkColumnTask(pipeBase.PipelineTask,
         outputZOffsets = defaultdict(lambda: defaultdict(dict))
         outputYTilts = defaultdict(lambda: defaultdict(dict))
         outputXTilts = defaultdict(lambda: defaultdict(dict))
+        outputRatioErrors = defaultdict(lambda: defaultdict(dict))
 
         badPixels = list(self.config.badMask)
 
@@ -162,7 +169,7 @@ class CrosstalkColumnTask(pipeBase.PipelineTask,
         zoffsetDict = defaultdict(lambda: defaultdict(list))
         ytiltDict = defaultdict(lambda: defaultdict(list))
         xtiltDict = defaultdict(lambda: defaultdict(list))
-
+        ratioErrorDict = defaultdict(lambda: defaultdict(list))
         extractedCount = 0
 
         for sourceAmp in sourceDetector:
@@ -208,6 +215,7 @@ class CrosstalkColumnTask(pipeBase.PipelineTask,
                 zoffsetDict[targetAmpName][sourceAmpName] = [float(results[1])]
                 ytiltDict[targetAmpName][sourceAmpName] = [float(results[2])]
                 xtiltDict[targetAmpName][sourceAmpName] = [float(results[3])]
+                ratioErrorDict[targetAmpName][sourceAmpName] = [float(results[4])]
                 extractedCount += 1
 
         self.log.info("Extracted %d pixels from %s -> %s",
@@ -216,13 +224,15 @@ class CrosstalkColumnTask(pipeBase.PipelineTask,
         outputZOffsets[targetChip][sourceChip] = zoffsetDict
         outputYTilts[targetChip][sourceChip] = ytiltDict
         outputXTilts[targetChip][sourceChip] = xtiltDict
+        outputRatioErrors[targetChip][sourceChip] = ratioErrorDict
         
         return pipeBase.Struct(
             outputRatios=ddict2dict(outputRatios),
             outputFluxes=ddict2dict(outputFluxes),
             outputZOffsets=ddict2dict(outputZOffsets),
             outputYTilts=ddict2dict(outputYTilts),
-            outputXTilts=ddict2dict(outputXTilts)
+            outputXTilts=ddict2dict(outputXTilts),
+            outputRatioErrors=ddict2dict(outputRatioErrors)
         )
 
 class CrosstalkSatelliteConnections(pipeBase.PipelineTaskConnections,
